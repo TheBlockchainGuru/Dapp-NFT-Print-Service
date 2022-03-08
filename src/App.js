cf09bf59d697e574750fc41cd6fcf908786994e8
J-Gambler <anotovserg943@gmail.com>
2022-03-08 22:13:01 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Checkout from './pages/Checkout/Checkout';
+import Choose from './pages/Choose/Choose';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/"></Route>
-          <Route></Route>
+          <Route path="/" element={ <Home /> }></Route>
+          <Route path="/checkout" element={ <Checkout /> }></Route>
+          <Route path="/choose" element={ <Choose /> }></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
