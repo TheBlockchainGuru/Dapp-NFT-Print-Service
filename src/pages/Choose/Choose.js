c3317ab327df89f2bc98f0466c59219a067d818f
J-Gambler <anotovserg943@gmail.com>
2022-03-08 22:35:22 +0300

    choose nft page basic layout

--- src/pages/Choose/Choose.js
+++ src/pages/Choose/Choose.js
@@ -2,7 +2,20 @@ import './Choose.scss';
 
 const Choose = () => {
     return (
-        <div className="choose"></div>
+        <div className="choose">
+            <div className="choose-title">NFT Prints</div>
+            <div class="choose-content">
+                <div className="choose-nft-collection">
+                    <select>
+                        <option value="">NFT Collection</option>
+                    </select>
+                </div>
+                <div className="choose-nft-individual">
+                    <input type="text" />
+                </div>
+                <div className="choose-nft-verify">Verify Ownerships</div>
+            </div>
+        </div>
     );
 }
 
