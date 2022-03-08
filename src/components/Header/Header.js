b44d2d85624b58df9514a97bf82249cd14f81b45
J-Gambler <anotovserg943@gmail.com>
2022-03-08 22:15:29 +0300

    header basic layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,24 @@
 import './Header.scss';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className="header"></div>
+        <div className="header">
+            <div className="logo">
+                <Link to="/">NFT Prints</Link>
+            </div>
+            <div className="links">
+                <div className="link">
+                    <Link to="/">Home</Link>
+                </div>
+                <div className="link">
+                    <Link to="/choose">Choose NFT</Link>
+                </div>
+                <div className="link">
+                    <Link to="/checkout">Checkout</Link>
+                </div>
+            </div>
+        </div>
     );
 }
 
