92be525d78e75c8d0ba81452e08731541134b2fe
J-Gambler <anotovserg943@gmail.com>
2022-03-10 17:25:35 +0300

    connect wallet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import './App.css';
+import Web3 from 'web3';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Checkout from './pages/Checkout/Checkout';
 import Choose from './pages/Choose/Choose';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [address, setAddress] = useState('');
+
+  useEffect(async () => {
+    await onConnect();
+  }, [] )
+
+  const onConnect = async () => {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable();
+      const accounts = await window.web3.eth.getAccounts();
+      setAddress(accounts[0])
+    } else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+      const accounts = await window.web3.eth.getAccounts();
+      setAddress(accounts[0])
+    } else {
+        window.alert('Non-Ethereum browser detected. Your should consider trying MetaMask!')
+    }
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
+        <Header connect={onConnect} address={address} />
         <Routes>
           <Route path="/" element={ <Home /> }></Route>
-          <Route path="/checkout" element={ <Checkout /> }></Route>
+          <Route 
+            path="/checkout" 
+            element={ 
+              <Checkout 
+                connect={onConnect} 
+                address={address}
+              /> }>
+          </Route>
           <Route path="/choose" element={ <Choose /> }></Route>
         </Routes>
         <Footer />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,18 @@
 import './Header.scss';
+import Web3 from 'web3';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import TogglerImage from '../../assets/images/toggler.png';
 
-const Header = () => {
+const Header = ({ connect, address }) => {
+
+    useEffect ( () => {
+    }, [address] )
+
+    const onConnectWallet = async () => {
+        await connect();
+    }
+
     return (
         <div className="header">
             <div className="logo">
@@ -18,6 +28,12 @@ const Header = () => {
                 <div className="link">
                     <Link to="/checkout">Checkout</Link>
                 </div>
+                <div className="link">
+                    {address
+                        ?   address
+                        :   <div className="connect" onClick={onConnectWallet}>Connect Wallet</div>
+                    }
+                </div>
             </div>
             <div className="collapse">
                 <div className="collapse-link-icon">
@@ -32,6 +48,9 @@ const Header = () => {
                     </div>
                     <div className="collapse-link">
                         <Link to="/checkout">Checkout</Link>
+                    </div>
+                    <div className="collapse-link">
+                        <div className="connect" onClick={onConnectWallet}>Connect Wallet</div>
                     </div>
                 </div>
             </div>
