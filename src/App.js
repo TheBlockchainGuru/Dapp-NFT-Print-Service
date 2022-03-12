057c4cc815892e5760806d4474223b17739fe51f
J-Gambler <anotovserg943@gmail.com>
2022-03-12 22:21:31 +0300

    add random contract selection

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ function App() {
             path="/choose" 
             element={ 
               <Choose 
+                address={address}
                 nft={nft}
                 setNft={e => onSetNFT(e)}
               /> }>
--- src/pages/Choose/Choose.js
+++ src/pages/Choose/Choose.js
@@ -1,20 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getNftMetadata, getNftsOfOwner } from '../../utils/alchemy';
+import { useMoralisWeb3Api } from "react-moralis";
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 import './Choose.scss';
+import 'react-notifications/lib/notifications.css';
 
 const contracts = require('../../config/nfts.json');
 
-const Choose = ({setNft}) => {
-
+const Choose = ({address, setNft}) => {
+    const Web3Api = useMoralisWeb3Api();
+    const colRef = useRef(null);
     const [collections, setCollections] 
             = useState([
-                <option value="" key={0}>NFT Collection</option>
+                { name: 'NFT Collection', value:"" }
             ])
     const [collection, setCollection] = useState('');
     const [tokenID, setTokenID] = useState('');
     const [collectionError, setCollectionError] = useState(false);
     const [tokenError, setTokenError] = useState(false);
+    const [contract, setContract] = useState("");
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate();
 
     useEffect( () => {
@@ -23,34 +29,44 @@ const Choose = ({setNft}) => {
         for(let i in contracts) {
             const oneContract = contracts[i];
 
-            newCollections.push(
-                                <option 
-                                    value={oneContract.contract} 
-                                    key={oneContract.contract}
-                                >
-                                    {oneContract.name}
-                                </option>
-                            )
+            newCollections
+                .push(
+                    { name: oneContract.name, value: oneContract.contract }
+                )
         }
 
         setCollections(newCollections)
     }, [] )
 
     const onVerify = async () => {
-
         if(!collection) {
             setCollectionError(true)
         } else if(!tokenID) {
             setTokenError(true)
         } else {
-            const nftData = await getNftMetadata(collection, tokenID)
-            if(!nftData) {
-                alert('Loading NFT information failed')
-            } else if(nftData.error) {
-                alert('Token does not exist')
+            // const ownedNfts = await getNftsOfOwner(address);
+            const ownedNfts = await getNftsOfOwner('0x9ab5a9572db323c70a94a1b043759b3490f6f51d');
+            const nftData   = await getNftMetadata(collection, tokenID);
+
+            if(!ownedNfts.ownedNfts.length) {
+                NotificationManager.error('You don\' have any NFT', 'Error message', 5000);
             } else {
-                setNft(nftData);
-                navigate('/checkout')
+                if(!nftData) {
+                    NotificationManager.error('Loading NFT information failed', 'Error message', 5000);
+                } else if(nftData.error) {
+                    NotificationManager.error('NFT does not exist', 'Error message', 5000);
+                } else {
+                    const nftIndex = ownedNfts.ownedNfts.findIndex(ele => 
+                        ele.contract.address == nftData.contract.address &&
+                        ele.id.tokenId == nftData.id.tokenId
+                    );
+                    if(nftIndex <= -1) {
+                        NotificationManager.error('That\' not your NFT', 'Error message', 5000);
+                    } else {
+                        setNft(nftData);
+                        navigate('/checkout')
+                    }
+                }    
             }
         }
     }
@@ -60,6 +76,8 @@ const Choose = ({setNft}) => {
 
         if(e.target.value) {
             setCollectionError(false);
+            setContract(e.target.value)
+
         } else {
             setCollectionError(true);
         }
@@ -75,14 +93,44 @@ const Choose = ({setNft}) => {
         }
     } 
 
+    const onChangeContract = async (e) => {
+        const selContract = e.target.value;
+        setContract(selContract)
+        const options = {
+            address: selContract,
+            chain: "eth",
+        }
+    
+        const metaData = await Web3Api.token.getNFTMetadata(options);
+
+        if(Object.keys(metaData).length !== 0) { 
+            // console.log(metaData)
+            const isExist = collections.findIndex( e => e.value == selContract )
+            if(isExist < 0) {
+                let newCollections = [...collections];
+                newCollections.push({name: metaData.name, value: selContract})
+                setCollections(newCollections)
+            }
+
+            colRef.current.value = selContract;
+            setCollection(selContract);
+        }
+    }
+
     return (
         <div className="choose">
+            <NotificationContainer/>
             <div className="choose-container">
                 <div className="choose-title">NFT Prints</div>
                 <div className="choose-content">
                     <div className="choose-nft-collection">
-                        <select onChange={onSelectCollection}>
-                            {collections}
+                        <select onChange={onSelectCollection} ref={colRef}>
+                        {collections.map ( e => 
+                            <option 
+                                value={e.value}
+                                key={e.value}
+                            >{e.name}</option>
+                        )}
                         </select>
                         <div 
                             style={{
@@ -91,8 +139,11 @@ const Choose = ({setNft}) => {
                             className="choose-nft-collection-error"
                         >This field is required.</div>
                     </div>
+                    <div className="choose-nft-wallet">
+                        <input type="text" placeholder="Enter wallet address" value={contract} onChange={onChangeContract} />
+                    </div>
                     <div className="choose-nft-individual">
-                        <input type="text" value={tokenID} onChange={onSetTokenID} />
+                        <input type="text" placeholder="Token ID" onChange={onSetTokenID} value={tokenID} />
                         <div 
                             style={{
                                 display: tokenError ? 'block' : 'none'
