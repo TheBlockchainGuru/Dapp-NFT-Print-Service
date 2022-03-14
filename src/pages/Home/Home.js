e7b6eea82e74d2de2e98aa5fcdfc659e98d7bcf6
J-Gambler <anotovserg943@gmail.com>
2022-03-14 15:02:19 +0300

    redirect when click the here

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Home.scss';
 
 const Home = () => {
@@ -8,8 +9,8 @@ const Home = () => {
                 <div className="home-content">
                     <div className="home-nft-code">
                         <div className="home-nft-count">
-                            <input type="number" />
-                            <div className="home-nft-label">If you don't have a code: Click <a href="" className="home-nft-label-highlight">Here</a></div>
+                            <input type="text" />
+                            <div className="home-nft-label">If you don't have a code: Click <Link to="/choose" className="home-nft-label-highlight">Here</Link></div>
                         </div>
                         <div className="home-nft-confirm">Enter</div>
                     </div>
